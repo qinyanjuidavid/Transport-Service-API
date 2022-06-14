cd380d7620910de77bbff996dd862991507be694
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-06-14 15:36:30 +0300

    configuring database connection

--- src/settings/production.py
+++ src/settings/production.py
@@ -21,20 +21,20 @@ DATABASES = {
 CORS_ALLOWED_ORIGINS = [
 
 ]
-# # SSL and TLS settings
-# CORS_REPLACE_HTTPS_REFERER = True
-# HOST_SCHEME = "https://"
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_HSTS_SECONDS = 1000000
-# SECURE_FRAME_DENY = True
+# SSL and TLS settings
+CORS_REPLACE_HTTPS_REFERER = True
+HOST_SCHEME = "https://"
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_SECONDS = 1000000
+SECURE_FRAME_DENY = True
 
-# db_from_env = dj_database_url.config()
-# DATABASES["default"].update(db_from_env)
-# DATABASES["default"]["CONN_MAX_AGE"] = 500
+db_from_env = dj_database_url.config()
+DATABASES["default"].update(db_from_env)
+DATABASES["default"]["CONN_MAX_AGE"] = 500
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
