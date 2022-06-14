0af82da34db562fe24239bf87b2163ec7dbc2b3a
qinyanjuidavid <davidkinyanjui052@gmail.com>
2022-06-14 12:33:50 +0300

    Gdal settings

--- src/settings/production.py
+++ src/settings/production.py
@@ -14,6 +14,8 @@ DEBUG = False
 #         'PORT': '5432'
 #     }
 # }
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 
 DATABASES = {
     'default': {
